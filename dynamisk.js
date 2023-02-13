49ecf3ba7ce966f91a1a89b66f2ac9e99f5d06f5
xLytje <rasmuslytje@gmail.com>
2023-02-13 08:30:03 +0100

    opg 1

---
+++ dynamisk.js
@@ -0,0 +1,29 @@
+// opg 1
+let nyPerson1 = {
+    firstname:  "Bjarne",
+    lastname:   "Hansen",
+    age:        "45",
+    country:    "Danmark",
+    gender:     "Mand"
+}
+let nyPerson2 = {
+    firstname:  "Arne",
+    lastname:   "Jensen",
+    age:        "48",
+    country:    "Finland",
+    gender:     "Mand"
+}
+let nyPerson3 = {
+    firstname:  "Brian",
+    lastname:   "Henrikson",
+    age:        "37",
+    country:    "Sverige",
+    gender:     "Mand"
+}
+let nyPerson4 = {
+    firstname:  "Per",
+    lastname:   "Nielsen",
+    age:        "65",
+    country:    "Norge",
+    gender:     "Mand"
+}
